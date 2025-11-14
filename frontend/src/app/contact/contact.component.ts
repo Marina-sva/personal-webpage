80915e69d6e50d3c18278c699f5af932ac4f352e
Marisha <marinasemenovaa1@gmail.com>
2025-11-14 21:26:49 +0100

    newprojet

---
+++ frontend/src/app/contact/contact.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-contact',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './contact.component.html',
+  styleUrl: './contact.component.css'
+})
+export class ContactComponent {
+  onSubmit() {
+    alert('Formular abgeschickt! (hier kannst du Backend-Logik hinzufügen)');
+  }
+
+}
