b90c6283b0787aeef8621106410929ed815308dd
Marisha <marinasemenovaa1@gmail.com>
2025-11-16 17:52:16 +0100

    feat:3 languges

--- frontend/src/app/header/header.component.ts
+++ frontend/src/app/header/header.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  currentLanguage: string;
+  dropdownOpen = false;
 
+  constructor(private translate: TranslateService) {
+    this.currentLanguage = translate.getDefaultLang() || 'de';
+  }
+
+  toggleDropdown() {
+    this.dropdownOpen = !this.dropdownOpen;
+  }
+
+  switchLanguage(lang: string) {
+    this.translate.use(lang);
+    this.currentLanguage = lang;
+    this.dropdownOpen = false;
+
+  }
 }
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { importProvidersFrom } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
