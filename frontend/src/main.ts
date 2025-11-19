88bd64c1fc41c313a88f50e36428cd4b4b1a77c7
Marisha <marinasemenovaa1@gmail.com>
2025-11-19 21:26:04 +0100

    deploy

--- frontend/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
-import { importProvidersFrom } from '@angular/core';
-import { TranslateModule } from '@ngx-translate/core';
-
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
