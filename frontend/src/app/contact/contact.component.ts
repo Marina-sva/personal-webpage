a7e4166a96b19e3b5f2f89c3155e34ac10ff6361
Marisha <marinasemenovaa1@gmail.com>
2025-11-29 21:16:26 +0100

    chor:contact-form

--- frontend/src/app/contact/contact.component.ts
+++ frontend/src/app/contact/contact.component.ts
@@ -1,16 +1,17 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, NgModule } from '@angular/core';
+import { FormsModule , NgForm} from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [FormsModule],
+  imports: [
+    CommonModule,
+    FormsModule ],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
-  onSubmit() {
-    alert('Formular abgeschickt! (hier kannst du Backend-Logik hinzufügen)');
-  }
 
 }
